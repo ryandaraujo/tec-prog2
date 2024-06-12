fd340f22bfded2af52e8475a6eacb2cdb35f6fc1
Ryan <ryanarajo@yahoo.com.br>
2024-06-11 21:18:16 -0300

    fix: correção de exibição do menu

--- atvii/src/menus/menuTipoListagemClientes.ts
+++ atvii/src/menus/menuTipoListagemClientes.ts
@@ -4,7 +4,7 @@ export default class MenuTipoListagemClientes implements Menu {
     mostrar(): void {
         console.clear()
         console.log(`****************************`)
-        console.log(`Qual o tipo de listagem desejada? `)
+        console.log(`Qual o tipo de listagem desejada?\n`)
         console.log(`1 - Todos os titulares`)
         console.log(`2 - Todos os dependentes`)
         console.log(`3 - Todos os dependentes de um titular específico`)
